de179923ecebd955d4f854734489459012b0ae4c
박가온누리 <gaonnuri@baggaonnuliui-MacBookAir.local>
2024-10-03 20:25:42 +0900

    first commit

---
+++ src/main/java/com/sparta/nuricalendar/NuriCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.nuricalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuriCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NuriCalendarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/nuricalendar/NuriCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.nuricalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuriCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
